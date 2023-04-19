cbdfff35ebccb089ce2a20696250a99e8fac4011
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-04-19 17:06:22 +0900

    [ADD] login logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
@@ -19,3 +21,21 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('posts:index')
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
