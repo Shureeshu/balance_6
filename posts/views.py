cf5116bd1c0966513935d36dd389368b11cee1d9
rloldl <you@example.comii319@naver.comgit config --global user.email you@example.comii319@naver.com>
2023-04-20 09:33:27 +0900

    [ADD] create logic

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def index(request):
     return render(request, 'posts/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:detail')
+
+    else:
+        form = PostForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create.html', context)
