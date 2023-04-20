04be141f8a6459980efb1f3a8b9f06c7e48ff96c
Hanui Jang <hany0147@naver.com>
2023-04-20 09:50:43 +0900

    [ADD] post detail

--- posts/views.py
+++ posts/views.py
@@ -4,15 +4,21 @@ from .forms import PostForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('posts:detail')
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('posts:detail', post.pk)
 
     else:
         form = PostForm()
@@ -21,3 +27,11 @@ def create(request):
         'form': form,
     }
     return render(request, 'posts/create.html', context)
+
+
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'posts/detail.html', context)
