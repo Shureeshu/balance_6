f6c55423a6a580a6da238a34520ab7940ce44463
Shureeshu <ruby04.s.lee@gmail.com>
2023-04-21 14:56:07 +0900

    pull main

--- posts/forms.py
+++ posts/forms.py
@@ -2,9 +2,50 @@ from django import forms
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(
+         label='', 
+         label_suffix='', 
+         widget=forms.TextInput(
+                attrs={
+                    'class': 'form-control', 
+                    'autofocus': True,
+                    'placeholder':"상황 설명(필수)",
+                }
+            )
+        )
+    select1_content = forms.CharField(
+         label='', 
+         label_suffix='', 
+         widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control text-wrap custom-select-form', 
+                    'autofocus': True,
+                    'placeholder':"선택지1(필수)",
+                }
+            )
+        )
+    select2_content = forms.CharField(
+         label='', 
+         label_suffix='', 
+         widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control text-wrap custom-select-form', 
+                    'autofocus': True,
+                    'placeholder':"선택지2(필수)",
+                }
+            )
+        )
+    img1 = forms.ImageField(
+         label='', 
+         label_suffix='', 
+        )
+    img2 = forms.ImageField(
+         label='', 
+         label_suffix='', 
+        )
     class Meta:
         model = Post
-        fields = ('title', 'select1_content','img1', 'select2_content', 'img2',)
+        fields = ('title', 'select1_content', 'select2_content', 'img1', 'img2',)
 
 
 class CommentForm(forms.ModelForm):
